07bc038e62fccce1229f5bd60b5791201ced868f
Luthfi Rahmad Susanto <luthfi.rahmad@payfazz.com>
2019-01-21 18:19:16 +0700

    create login feature

---
+++ features/login.feature
@@ -0,0 +1,13 @@
+#DI file feature, kalian membuat testcase
+Feature: This is login for Payfazz application
+  Scenario: I want to test login feature in existing payfazz account
+    #Step give menyala karena steps_definiton belum diajabarkan
+    #untuk menjabarkan, ketik cucumber pada terminal
+    Given I land on onboarding page
+    Then I click on Login button
+    Then I fill login number
+    Then I click Lanjut button
+    Then I fill login password
+    Then I click on Login button to enter Home page
+    Then I land on Home page
+
---
+++ features/steps_definitions/login_steps.rb
@@ -0,0 +1,27 @@
+Given("I land on onboarding page") do
+ puts "Cek gambar di halaman Onboarding"
+end
+
+Then("I click on Login button") do
+  puts "Saya menekan tombol Login"
+end
+
+Then("I fill login number") do
+  puts "Saya mengisi nomor 081517938164"
+end
+
+Then("I click Lanjut button") do
+  puts "Saya menekan tombol Lanjut"
+end
+
+Then("I fill login password") do
+  puts "Saya mengisi password Ultraman123"
+end
+
+Then("I click on Login button to enter Home page") do
+  puts "Saya menekan tombol Login"
+end
+
+Then("I land on Home page") do
+  puts "Saya berhasil masuk ke halaman Home"
+end
